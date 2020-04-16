08e8c553c2b96eba3c40e41b7133e81f3b74ae97
Antoine <antoinroux@hotmail.fr>
2020-04-16 20:11:16 +0200

    Fix TCPServer server_close() method implementation
    Call parent method shutdown() to stop BaseServer serve_forever() loop

--- Lib/socketserver.py
+++ Lib/socketserver.py
@@ -480,6 +480,8 @@ class TCPServer(BaseServer):
         May be overridden.
 
         """
+        # stop infinite loop BaseServer.serve_forever method
+        super().shutdown()
         self.socket.close()
 
     def fileno(self):
